3ba388b42f801ab14fe08e4ad2186c4fbc5dcf7f
martin <sgzs6721@126.com>
2014-08-06 10:54:42 +0800

    fix upload images base url

--- js/ckfinder/config.php
+++ js/ckfinder/config.php
@@ -60,7 +60,7 @@ Examples:
 
 ATTENTION: The trailing slash is required.
 */
-$baseUrl = '/Immigration/Upload/Images/';
+$baseUrl = '/Immigration/js/ckfinder/userfiles/';
 
 /*
 $baseDir : the path to the local directory (in the server) which points to the
