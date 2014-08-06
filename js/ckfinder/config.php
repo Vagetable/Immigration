508c3a28bb242fe364d886adcd4c2365e2ebc035
martin <sgzs6721@126.com>
2014-08-06 10:49:38 +0800

    change upload image path

--- js/ckfinder/config.php
+++ js/ckfinder/config.php
@@ -60,7 +60,7 @@ Examples:
 
 ATTENTION: The trailing slash is required.
 */
-$baseUrl = '/Immigration/js/ckfinder/userfiles/';
+$baseUrl = '/Immigration/Upload/Images/';
 
 /*
 $baseDir : the path to the local directory (in the server) which points to the
