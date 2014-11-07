42daaf3c19e6482a9b1266d15934181d2c49eb34
martin <sgzs6721@126.com>
2014-11-07 15:45:11 +0800

    modify

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -4,7 +4,7 @@ header("Content-Type: text/html;charset=utf-8");
 class Test extends MY_Controller{
 	public function index()
 	{
-		$data_array = array('name' => 'lei.wang', );
+		$data_array = array('name' => 'This is a test', );
 		$this->cismarty->view('test.tpl',$data_array);
 	}
 	public function testadd()
